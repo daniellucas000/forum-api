2c7a42dae86645035b774b6e960d949738d91b96
Daniel <danielluccas000@gmail.com>
2026-07-20 19:13:22 -0300

    feat: integrate Prism as ORM and refator database/auth/user - Add prism.config.ts, schema.prism and initial migration - Cria DatabaseModule and PrismaService (com testes) - Upgrade AuthController/Module/Service to use Prism - Upgrade UserController/Module/Service to use Prism - Adjust tsconfig.json and tsconfig.spec.json - Upgrade bootstrap of the application (main.ts, app.module.ts)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { User } from 'generated/prisma/client';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async signIn(
+    email: string,
+    password: string,
+  ): Promise<Omit<User, 'password'>> {
+    const user = await this.userService.user({ email: email });
+
+    if (!user) {
+      throw new NotFoundException(`User not found`);
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: _password, ...result } = user;
+
+    return result;
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Prisma, User as UserModel } from 'generated/prisma/client';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  async signupUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<UserModel> {
+    const user = await this.userService.user({ id: Number(id) });
+
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+
+    return user;
+  }
+
+  @Put()
+  async updateUser(
+    @Body() userData: Prisma.UserUpdateInput,
+    @Param('id') id: string,
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: userData,
+    });
+  }
+
+  @Delete()
+  async deleteUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id: Number(id) });
+  }
+}
